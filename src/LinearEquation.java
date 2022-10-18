48b0d13eb1e85cac4319dcf7a0e48240d71e1089
Shamsrary@gmail.com <Shamsrary@gmail.com>
2022-10-18 12:33:33 -0400

    progress

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class LinearEquation {
+public class LinearEquation {
+    private String coordinate1;
+    private String coordinate2;
+
+
+    public LinearEquation(String coordinate1, String coordinate2){
+        this.coordinate1 = coordinate1;
+        this.coordinate2 = coordinate2;
+
+    }
+
+
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class LinearEquationRunner {
+import java.util.Scanner;
+
+public class LinearEquationRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome ");
+        //gains the coordinate info for the slope.
+        System.out.println("Enter coordinate 1 ");
+        String num1 = scan.nextLine();
+        System.out.println("Enter coordinate 2 ");
+        String num2 = scan.nextLine();
+        System.out.println(num1);
+        // obtains the first digit to the last digit for the specific number and converts to integer
+        int num1x = Integer.parseInt(num1.substring(1, num1.indexOf(",")));
+        int num2x = Integer.parseInt(num1.substring(num1.indexOf(" ") + 1, num1.indexOf(")")));
+        int num1y = Integer.parseInt(num2.substring(1, num2.indexOf(",")));
+        int num2y = Integer.parseInt(num2.substring(num2.indexOf(" ") + 1, num2.indexOf(")")));
+
+        //LinearEquation first = new LinearEquation(num1, num2);
+        //String check = first.lol();
+        System.out.print(num1);
+    }
 }
