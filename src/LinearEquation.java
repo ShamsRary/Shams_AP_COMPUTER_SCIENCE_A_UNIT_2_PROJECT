a7982f844567a359f142c0258e7ab0daee33d058
Shamsrary@gmail.com <Shamsrary@gmail.com>
2022-10-19 12:10:55 -0400

    progress

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,13 +1,49 @@
 public class LinearEquation {
-    private String coordinate1;
-    private String coordinate2;
+    private int coordinate1x;
+    private int coordinate1y;
+    private int coordinate2x;
+    private int coordinate2y;
 
 
-    public LinearEquation(String coordinate1, String coordinate2){
-        this.coordinate1 = coordinate1;
-        this.coordinate2 = coordinate2;
+    public LinearEquation(int coordinate1x, int coordinate1y, int coordinate2x, int coordinate2y){
+        this.coordinate1x = coordinate1x;
+        this.coordinate1y = coordinate1y;
+        this.coordinate2x = coordinate2x;
+        this.coordinate2y = coordinate2y;
 
     }
+
+    public void lineInfo()
+    {
+        if (coordinate1x != coordinate2x) {
+            System.out.println("The coordinates are " + "(" + coordinate1x + ", " + coordinate1y + ")" + " and " + "(" + coordinate2x + ", " + coordinate2y + ")");
+            double slope = ((double) coordinate2y - (double) coordinate1y) / ((double) coordinate2x - (double) coordinate1x);
+            double y_intercept = ((double) coordinate1y) - ((slope * (double) coordinate1x));
+            double distance = Math.sqrt(Math.pow((coordinate2x - coordinate1x), 2) + Math.pow((coordinate2y - coordinate1y), 2));
+            System.out.println("These coordinates make an equation of: y = " + slope + "x" + "+" + y_intercept);
+            System.out.println("The slope is " + slope);
+            System.out.println("The y_intercept is " + y_intercept);
+            System.out.println("The distance of the 2 points is " + distance);
+        }
+        else{
+            System.out.println("Vertical line x = " + coordinate1x);
+        }
+
+    }
+
+    public void pointer(double xCord){
+        if (coordinate1x != coordinate2x) {
+            double slope = ((double) coordinate2y - (double) coordinate1y) / ((double) coordinate2x - (double) coordinate1x);
+            double y_intercept = ((double) coordinate1y) - ((slope * (double) coordinate1x));
+            double yCord = slope * xCord + y_intercept;
+            System.out.println("(" + xCord + ", " + yCord + ")");
+        }
+        else{
+            System.out.println("Vertical line-- point on line is still = " + coordinate1x);
+        }
+    }
+
+
 
 
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -9,15 +9,23 @@ public class LinearEquationRunner {
         String num1 = scan.nextLine();
         System.out.println("Enter coordinate 2 ");
         String num2 = scan.nextLine();
-        System.out.println(num1);
+
         // obtains the first digit to the last digit for the specific number and converts to integer
         int num1x = Integer.parseInt(num1.substring(1, num1.indexOf(",")));
-        int num2x = Integer.parseInt(num1.substring(num1.indexOf(" ") + 1, num1.indexOf(")")));
-        int num1y = Integer.parseInt(num2.substring(1, num2.indexOf(",")));
+        int num1y = Integer.parseInt(num1.substring(num1.indexOf(" ") + 1, num1.indexOf(")")));
+        int num2x = Integer.parseInt(num2.substring(1, num2.indexOf(",")));
         int num2y = Integer.parseInt(num2.substring(num2.indexOf(" ") + 1, num2.indexOf(")")));
 
-        //LinearEquation first = new LinearEquation(num1, num2);
-        //String check = first.lol();
-        System.out.print(num1);
+//        test code
+//        System.out.println(num1x);
+//        System.out.println(num1y);
+//        System.out.println(num2x);
+//        System.out.println(num2y);
+
+        LinearEquation test1 = new LinearEquation(num1x, num1y, num2x, num2y);
+        test1.lineInfo();
+        System.out.println("Enter a value for x: ");
+        double point = scan.nextDouble();
+        test1.pointer(point);
     }
 }
